78293b5b3be374dabeda57c750ed6e9d198e515d
Harith Onigemo <45686269+Harithmetic1@users.noreply.github.com>
2020-11-28 12:37:52 +0100

    Buycoins Project

---
+++ Assets/script.js
@@ -0,0 +1,7 @@
+let dropdown = document.querySelector('.dropdown');
+
+//function for the menu icon functionality
+function dropdownButton(){
+    dropdown.classList.toggle("block");
+    dropdown.classList.toggle("none");
+}
